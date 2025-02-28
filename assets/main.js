51741934e16f0418c6bd39dbeab04ba99c14934a
h3110 <desenvolvimento15@dl.com.br>
2025-02-28 08:37:35 -0300

    vs01

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+  $(document).ready(function(){
+    $('.slider1').slick({
+      slidesToShow: 2,  // Mostra 3 slides por vez (ajuste se necessário)
+      slidesToScroll: 1, // Avança 1 slide por vez
+      autoplaySpeed: 5000,
+      autoplay: true, 
+      dots: false, 
+      arrows: false,
+      infinite: true, // Garante o loop infinito
+      cssEase: 'linear',
+      variableWidth: true// Evita que os slides tenham larguras variáveis
+    });
+  });
+
